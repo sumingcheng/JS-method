52c73b0535086258b5e3942e8f94fde38898382b
fanxiaohao <fanxiaohao@mail.taiji.com.cn>
2023-02-24 16:29:21 +0800

    1

--- Leetcode/合并两个有序链表.js
+++ Leetcode/合并两个有序链表.js
@@ -1,21 +1,17 @@
 let l1 = [1, 2, 4], l2 = [1, 3, 4];
-
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
 var mergeTwoLists = function (l1, l2) {
-  let list = new ListNode() // 初始化
-  function merge(i, j, pre){
-    if(i === null || j === null) { // 结束的条件
-      pre.next = i ? i : j // 该结束了，谁还剩余，就把谁贴上去
-      return list.next
-    }
-    if (i.val < j.val) {
-      pre.next = new ListNode(i.val)
-      return merge(i.next, j, pre.next)
-    } else {
-      pre.next = new ListNode(j.val)
-      return merge(i, j.next, pre.next)
-    }
+  if (l1 === null) {
+    return l2;
+  } else if (l2 === null) {
+    return l1;
+  } else if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
   }
-  return merge(list1, list2, list)
 };
 
 console.log(mergeTwoLists(l1, l2));
