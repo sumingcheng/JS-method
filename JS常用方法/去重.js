3f508c6a718cf56087647a7c5541aa1ca2240069
Sumingcheng <fanxiaohaohaha@qq..com>
2023-02-28 21:58:38 +0800

    去重

--- JS常用方法/去重.js
+++ JS常用方法/去重.js
@@ -1,14 +1,17 @@
 let arr = [1, 2, 3, 4, 3, 3, 2, 5]
 let newArr1 = [...new Set(arr)]
 console.log(newArr1)
+
 // Set
 let newArr2 = Array.from(new Set(arr))
 console.log(newArr2)
+
 // filter + indexOf
 let newArr3 = arr.filter((value, index) => {
   return arr.indexOf(value) === index
 })
 console.log(newArr3)
+
 // Map
 let newArr4 = (arr) => {
   let map = new Map()
