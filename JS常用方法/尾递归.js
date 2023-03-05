6325f402dbc929bfbcb7f9cdeab45466b5e85d1b
Sumingcheng <fanxiaohaohaha@qq..com>
2023-03-06 07:47:11 +0800

    2023年3月6日 07:47:07

--- JS常用方法/尾递归.js
+++
@@ -1,7 +0,0 @@
-function factorial(n, acc = 1) {
-  if (n === 0) {
-    return acc
-  } else {
-    return factorial(n - 1, n * acc)
-  }
-}
--- JS常用方法/数组拍平.js
+++ JS常用方法/数组拍平.js
@@ -1,3 +1,27 @@
 const arr = [1, 2, [3, 4, [5, 6]]]
 const flatArr = arr.flat(2)
 console.log(flatArr)
+
+// 判断数组层数
+function getNestedLevel(arr) {
+  let level = 1 // 初始化层数为1
+
+  // 递归函数
+  function getLevel(arr, curLevel) {
+    // 遍历数组
+    arr.forEach(item => {
+      if (Array.isArray(item)) {
+        // 如果当前元素仍然是数组，递归调用函数
+        getLevel(item, curLevel + 1)
+      } else {
+        // 如果当前元素不是数组，更新层数的最大值
+        level = Math.max(level, curLevel)
+      }
+    })
+  }
+
+  // 调用递归函数
+  getLevel(arr, 1)
+
+  return level
+}
